e464a716aacfeee65262e9e0abf0bdb236f88c08
newbie060 <poozagrg432@gmail.com>
2026-03-16 20:03:04 +0545

    fix: fixed api error route

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -2,38 +2,45 @@ import { db } from "@/config/db";
 import { chatTable, frameTable, projectTable, usersTable } from "@/config/schema";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
 
 export async function POST(req: NextRequest) {
-    const {projectId, frameId, messages, credits} = await req.json();
-    const user = await currentUser();
-    const {has} = await auth();
-    const hasUnlimitedCredits = has&&has({ plan: 'unlimited'});
 
-    const projectResult = await db.insert(projectTable).values({
+    const { projectId, frameId, messages, credits } = await req.json();
+
+    const user = await currentUser();
+    const { has } = await auth();
+
+    const hasUnlimitedCredits = has&&has({ plan: "unlimited" });
+
+    await db.insert(projectTable).values({
         projectId: projectId,
         createdBy: user?.primaryEmailAddress?.emailAddress
-    })
+    });
 
-    const frameResult = await db.insert(frameTable).values({
+    await db.insert(frameTable).values({
         frameId: frameId,
         projectId: projectId,
-    })
+    });
 
-    const chatResult = await db.insert(chatTable).values({
+    await db.insert(chatTable).values({
         frameId: frameId,
-        projectId: projectId,
         chatMessage: messages,
         createdBy: user?.primaryEmailAddress?.emailAddress
-    })
+    });
 
-    if(!hasUnlimitedCredits) {
-    const userResult = await db.update(usersTable).set({
-        credits: credits-1
-        //@ts-ignore
-    }).where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress))
+    if (!hasUnlimitedCredits) {
+        await db.update(usersTable)
+            .set({
+                credits: credits - 1
+            })
+            //@ts-ignore
+            .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress));
+    }
 
     return NextResponse.json({
-        projectId, frameId, messages
-    })
-}
+        projectId,
+        frameId,
+        messages
+    });
 }
